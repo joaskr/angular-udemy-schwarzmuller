83ac6a060a2c6dc8a275426ce52bfdc8bf208644
jasg <joanna.skrzypczak@gft.com>
2022-05-25 11:59:17 +0200

    finished section 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountsService} from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  serverElements = [{type: 'server', name: 'testserver', content: 'test'}];
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
+export class AppComponent implements OnInit {
+  accounts: {name: string, status: string}[] = [];
+  constructor(private accountService: AccountsService) {
   }
-  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverName
-    });
+  ngOnInit() {
+    this.accounts = this.accountService.accounts;
   }
 }
